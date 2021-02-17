351922822e4f93ed05f9e75d1f77b8e8da84b55f
djimo-h <hocine.djimo@gmail.com>
2021-02-17 15:57:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vuejs-starter-1bfq8t
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vuejs-starter-1bfq8t)
---
+++ index.js
@@ -0,0 +1,10 @@
+import Vue from "vue";
+import { App } from "./app";
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    product: "Socks",
+    image: "./assets/vmSocks-green.jpg"
+  }
+});
